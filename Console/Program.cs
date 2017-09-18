4465e09ffe3f27306e7c52d422aa1797e5506d04
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-09-17 20:22:48 -0500

    Testing console app

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,17 +1,29 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Repository;
 
-namespace Console
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ICalendarRepository calendarRepository = new O365CalendarRepository();
-            INewsRepository newsRepository = new NYTRepository();
-            ITransportationRepository transportationRepository = new MetraRepository();
-            IWeatherRepository weatherRepository = new WundergroundRepository();
+            // https://wiki.saucelabs.com/display/DOCS/Best+Practice%3A+Use+Environment+Variables+for+Authentication+Credentials
+            //System.Collections.IDictionary dictionary = Environment.GetEnvironmentVariables();
+
+            //ICalendarRepository calendarRepository = new O365CalendarRepository();
+            INewsRepository newsRepository = new Repository.NYT.NYTRepository(Environment.GetEnvironmentVariable("HDB_NYT_KEY"));
+            //ITransportationRepository transportationRepository = new MetraRepository();
+            IWeatherRepository weatherRepository = new Repository.Wunderground.WundergroundRepository(Environment.GetEnvironmentVariable("HDB_NYT_KEY"));
+
+            Task.Run(async () =>
+            {
+                //IEnumerable<Repository.Models.NewsItem> newsItems = await newsRepository.GetNewsItems();
+                Repository.Models.WeatherData weather = await weatherRepository.GetWeather("60532");
+            });
+
+            Console.ReadKey(false);
         }
     }
 }
